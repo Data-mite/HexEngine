4e89876767d0731632dcd991344ecd6d0b2ed952
Data-mite <88356818+Data-mite@users.noreply.github.com>
2025-06-03 21:02:32 -0400

    On

--- test_path.py
+++
@@ -1,8 +0,0 @@
-import json
-from path import generate_path
-from graph import render_path_with_networkx
-
-if __name__ == "__main__":
-    path = generate_path(num_tiles=10)
-    print(json.dumps(path, indent=2))
-    render_path_with_networkx(path)
